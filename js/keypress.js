79040029da20b2b39e0795b04d130cd82318412b
Daniel Aguado Huaccharaqui <daniel030891@gmail.com>
2019-09-23 02:11:50 -0500

    Se agrego Proyecto KeyPress

---
+++ js/keypress.js
@@ -0,0 +1,10 @@
+var divElement = document.getElementById('viz1569220068530');                    
+var vizElement = divElement.getElementsByTagName('object')[0];                    
+vizElement.style.minWidth='420px';
+// vizElement.style.maxWidth='100%';
+vizElement.style.minHeight='587px';
+vizElement.style.width='100%';
+vizElement.style.maxHeight=(divElement.offsetWidth*0.75)+'px';         
+var scriptElement = document.createElement('script');                    
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
